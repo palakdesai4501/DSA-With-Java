523aada1ad3afd7c0625e2fc4a293fce3130bff2
Palak <palak.desai4501@gmail.com>
2023-07-08 10:40:51 +0530

    Feature: Find Sqrt of a Number With Precision p

---
+++ maths12.java
@@ -0,0 +1,43 @@
+// Sqrt of a number with precision p using binary search and brute force approach in O(log(n)) time complexity 
+public class maths12 {
+    public static void main(String[] args) {
+        int n=40;
+        int p=3;
+
+        System.out.printf("%.3f", sqrt(n,p));
+    }
+
+    //o(log(n)) - time complexity
+    static double sqrt(int n, int p){
+        int s=0;
+        int e=n;
+
+        double root =0.0;
+
+        while(s<=e){
+            int m= s+ (e-s)/2;
+
+            if(m*m == n){
+                root =m;
+                return m;
+            }
+
+            if(m*m > n){
+                e=m-1;
+            }else{
+                s=m+1;
+            }
+        }
+
+        double incr = 0.1;
+        for(int i=0; i<p;i++){
+            while(root*root<=n){
+                root+=incr;
+            }
+
+            root-=incr;
+            incr/=10;
+        }
+        return root;
+    }
+}
