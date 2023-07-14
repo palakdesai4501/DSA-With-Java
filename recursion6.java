e6ad79e0461c1fb14a71bc9f2765d148c8d095e8
Palak <palak.desai4501@gmail.com>
2023-07-14 12:13:55 +0530

    Feature: Factorial Of a Number Using Recursion

---
+++ recursion6.java
@@ -0,0 +1,16 @@
+// Factorial of a number
+public class recursion6 {
+    public static void main(String[] args) {
+        int ans = fact(5);
+        System.out.println(ans);
+    }
+
+    static int fact(int n){
+        if(n<=1){
+            return 1;
+        }
+
+        return n * fact(n-1);
+
+    }
+}
