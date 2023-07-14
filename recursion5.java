89d827eb15bfbe4555f09b063108735032fb4f43
Palak <palak.desai4501@gmail.com>
2023-07-14 12:13:13 +0530

    Feature: print n to 1 & 1 to n Using Recursion

---
+++ recursion5.java
@@ -0,0 +1,17 @@
+//print n to 1 and 1 to n both
+public class recursion5 {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+
+    static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
+
