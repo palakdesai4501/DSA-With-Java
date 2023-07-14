ac1be30e1142c7272725cf29c74e689e12f83ccb
Palak <palak.desai4501@gmail.com>
2023-07-14 11:30:13 +0530

    Feature: Fibonacci Number Using Recursion

---
+++ recursion1.java
@@ -0,0 +1,30 @@
+// Fibonacci number program using recursion
+public class recursion1 {
+    public static void main(String[] args) {
+        for(int i=0; i < 11; i++){
+            System.out.println(fiboFormula(i));
+        }
+    }
+
+    /**
+     * This is the formula for calculating the nth fibonacci number
+     * @param n
+     * @return nth fibonacci number
+     */
+    static int fiboFormula(int n){
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
+    }
+
+    /**
+     * This is the recursive function for calculating the nth fibonacci number
+     * @param n
+     * @return nth fibonacci number
+     */
+    static int fibo(int n){
+
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
