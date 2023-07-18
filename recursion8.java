d00bc0f0c2b0e8e08bc3d7652fb825a41fd087aa
Palak <palak.desai4501@gmail.com>
2023-07-18 09:47:29 +0530

    Feature: Product Of Digits Of a Number Using Recursion

---
+++ recursion8.java
@@ -0,0 +1,15 @@
+// Product of digits of a number using recursion
+public class recursion8 {
+    public static void main(String[] args) {
+        int ans = prod(1342);
+        System.out.println(ans);
+    }
+
+    static int prod(int n){
+        if(n%10==n){
+            return n;
+        }
+
+        return (n%10) * prod(n/10);
+    }
+}
