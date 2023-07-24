0c5fd0ad434bce4778ecd57016cef4ea0ac3d992
Palak <palak.desai4501@gmail.com>
2023-07-24 10:20:10 +0530

    Feature: Skip Character in a String

---
+++ recursion20.java
@@ -0,0 +1,45 @@
+// Skip character in a string 
+// input: baccdah
+// output: bcdh
+public class recursion20 {
+    public static void main(String[] args) {
+        System.out.println(skip("baccdah"));
+    }
+
+    /**
+     * skip character in a string
+     * @param p
+     * @param up
+     */
+    static void skip(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+            skip(p, up.substring(1));
+        } else {
+            skip(p + ch, up.substring(1));
+        }
+    }
+
+    /**
+     * skip character in a string
+     * @param up
+     * @return
+     */
+    static String skip(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if (ch == 'a') {
+           return skip(up.substring(1));
+        } else {
+            return ch + skip(up.substring(1));
+        }
+    }
+}
