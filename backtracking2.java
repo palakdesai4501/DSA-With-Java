3bdc0b97f15e6eba462b9c01f5b4f89613d0c24e
Palak <palak.desai4501@gmail.com>
2023-07-28 09:57:03 +0530

    Feature: N-queen problem

---
+++ backtracking2.java
@@ -0,0 +1,75 @@
+// N-queens problem
+// Algorithm:
+// 1. place the queen in the first row and check for every column
+// 2. if the queen is safe, place it and move to the next row
+// 3. if the queen is not safe, move to the next column
+// 4. if all the columns are checked, move to the previous row and move to the next column
+// 5. if all the rows are checked, return
+// time complexity: o(N^3 + N!) = o(N!)
+public class backtracking2 {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean[][] board = new boolean[4][4];
+        System.out.println(queens(board, 0));
+    }
+
+    static int queens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+
+        // placing the queen and checking for every row and col
+        for (int col = 0; col < board.length; col++) {
+            // place the queen if it is safe
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // check vertical row
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        // diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        // diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean element : row) {
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
