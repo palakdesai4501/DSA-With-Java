a78eae5b80cac4d938d671a8cccd7f07cdafc394
Palak <palak.desai4501@gmail.com>
2023-09-03 22:20:40 +0530

    Feature: Check valid paranthesis using stack

---
+++ StackValidParenthesis.java
@@ -0,0 +1,36 @@
+// Check valid paranthesis using stack 
+// input: ()[]{}, output: true 
+// input: ([)], output: false
+// input: {[]}, output: true
+import java.util.Stack;
+
+public class StackValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (ch == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
+
+                }
+                if (ch == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                if (ch == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
