cf1b2173bb30a1963da665c73371e21fc54ead95
Palak <palak.desai4501@gmail.com>
2023-10-31 18:20:08 +0530

    Feature: Program to implement binary tree

---
+++ Trees/main1.java
@@ -0,0 +1,13 @@
+// implement binarytree
+package Trees;
+
+import java.util.Scanner;
+
+public class main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.prettyDisplay();
+    }
+}
