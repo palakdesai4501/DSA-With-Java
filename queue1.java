8c1f1ad3a27799ea63909d70771cda2b075e62b3
Palak <palak.desai4501@gmail.com>
2023-10-31 10:47:53 +0530

    Feature: Program to return head of the queue and removes it

---
+++ queue1.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queue1 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(3);
+        queue.add(6);
+        queue.add(5);
+        queue.add(19);
+        queue.add(1);
+
+        System.out.println(queue.peek());
+       // System.out.println(queue.remove()); 
+       // Returns the head of the queue and removes it
+    }
+}
