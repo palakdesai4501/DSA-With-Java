7eb9bf04380f0b44bda6d6c4aca556c27a18b907
Palak <palak.desai4501@gmail.com>
2023-11-07 11:46:05 +0530

    Feature: Program to run BST

---
+++ Trees/main2.java
@@ -0,0 +1,10 @@
+package Trees;
+
+public class main2 {
+    public static void main(String[] args) {
+        BST tree = new BST();
+        int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+        tree.populate(nums);
+        tree.display();
+    }
+}
