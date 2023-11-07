76e76782f10aff09a8c38c829a05e893af6983ae
Palak <palak.desai4501@gmail.com>
2023-11-07 11:38:24 +0530

    Feature: Created class node and getValue function

---
+++ Trees/BST.java
@@ -0,0 +1,19 @@
+// Program to display BST
+package Trees;
+
+public class BST {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
