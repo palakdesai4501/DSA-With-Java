0044df36f60d901f60674be735a133aea83a666e
Palak <palak.desai4501@gmail.com>
2023-11-07 11:39:55 +0530

    Feature: Created insert and height method

--- Trees/BST.java
+++ Trees/BST.java
@@ -16,4 +16,39 @@ public class BST {
             return value;
         }
     }
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+          return -1;
+        }
+        return node.height;
+      }
 }
