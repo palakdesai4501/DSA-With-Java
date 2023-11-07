4cefff7c6004be3af536c598be6d030521e75e2c
Palak <palak.desai4501@gmail.com>
2023-11-07 13:53:30 +0530

    Feature: Trees Program

--- Trees/BST.java
+++ Trees/BST.java
@@ -1,104 +1,104 @@
-// Program to display BST
 package Trees;
 
 public class BST {
-    public class Node {
-        private int value;
-        private Node left;
-        private Node right;
-        private int height;
+  public class Node {
+    private int value;
+    private Node left;
+    private Node right;
+    private int height;
 
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
+    public Node(int value) {
+      this.value = value;
     }
 
-    private Node root;
+    public int getValue() {
+      return value;
+    }
+  }
 
-    public BST() {
+  private Node root;
 
+  public BST() {
+
+  }
+
+  public void insert(int value) {
+    root = insert(value, root);
+  }
+
+  private Node insert(int value, Node node) {
+    if (node == null) {
+      node = new Node(value);
+      return node;
     }
 
-    public void insert(int value) {
-        root = insert(value, root);
+    if (value < node.value) {
+      node.left = insert(value, node.left);
     }
 
-    private Node insert(int value, Node node) {
-        if (node == null) {
-            node = new Node(value);
-            return node;
-        }
-
-        if (value < node.value) {
-            node.left = insert(value, node.left);
-        }
-
-        if (value > node.value) {
-            node.right = insert(value, node.right);
-        }
-
-        node.height = Math.max(height(node.left), height(node.right)) + 1;
-        return node;
+    if (value > node.value) {
+      node.right = insert(value, node.right);
     }
 
-    public void populate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            this.insert(nums[i]);
-        }
+    node.height = Math.max(height(node.left), height(node.right)) + 1;
+    return node;
+  }
+
+  public void populate(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+      this.insert(nums[i]);
+    }
+  }
+
+  public void populatedSorted(int[] nums) {
+    populatedSorted(nums, 0, nums.length);
+  }
+
+  private void populatedSorted(int[] nums, int start, int end) {
+    if (start >= end) {
+      return;
     }
 
-    public void populatedSorted(int[] nums) {
-        populatedSorted(nums, 0, nums.length);
+    int mid = (start + end) / 2;
+
+    this.insert(nums[mid]);
+    populatedSorted(nums, start, mid);
+    populatedSorted(nums, mid + 1, end);
+  }
+
+  public void display() {
+    display(this.root, "Root Node: ");
+  }
+
+  private void display(Node node, String details) {
+    if (node == null) {
+      return;
     }
+    System.out.println(details + node.value);
+    display(node.left, "Left child of " + node.value + " : ");
+    display(node.right, "Right child of " + node.value + " : ");
+  }
 
-    private void populatedSorted(int[] nums, int start, int end) {
-        if (start >= end) {
-            return;
-        }
+  public boolean isEmpty() {
+    return root == null;
+  }
 
-        int mid = (start + end) / 2;
-
-        this.insert(nums[mid]);
-        populatedSorted(nums, start, mid);
-        populatedSorted(nums, mid + 1, end);
+  public int height(Node node) {
+    if (node == null) {
+      return -1;
     }
+    return node.height;
+  }
 
-    public void display() {
-        display(this.root, "Root Node: ");
-    }
+  public boolean balanced() {
+    return balanced(root);
+  }
 
-    private void display(Node node, String details) {
-        if (node == null) {
-            return;
-        }
-        System.out.println(details + node.value);
-        display(node.left, "Left child of " + node.value + " : ");
-        display(node.right, "Right child of " + node.value + " : ");
+  private boolean balanced(Node node) {
+    if (node == null) {
+      return true;
     }
+    return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+  }
 
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    public int height(Node node) {
-        if (node == null) {
-            return -1;
-        }
-        return node.height;
-    }
-
-    public boolean balanced() {
-        return balanced(root);
-    }
-
-    private boolean balanced(Node node) {
-        if (node == null) {
-            return true;
-        }
-        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
-    }
 }
