af5232ff2210f82b7ca97236b79f8b5b8fd79f3b
Palak <palak.desai4501@gmail.com>
2023-11-18 13:40:21 +0530

    Feature: Added rightRotate Function

--- Trees/AVL.java
+++ Trees/AVL.java
@@ -51,26 +51,40 @@ public class AVL {
                 // left left case
                 return rightRotate(node);
             }
-            if(height(node.left.left) - height(node.left.right) < 0) {
+            if (height(node.left.left) - height(node.left.right) < 0) {
                 // left right case
                 node.left = leftRotate(node.left);
                 return rightRotate(node);
-              }
+            }
         }
 
         if (height(node.left) - height(node.right) < -1) {
             // right heavy
-            if(height(node.right.left) - height(node.right.right) < 0) {
-              // right right case
-              return leftRotate(node);
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
+                return leftRotate(node);
             }
 
-            if(height(node.right.left) - height(node.right.right) > 0) {
-                // left right case
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // right left case
                 node.right = rightRotate(node.right);
                 return leftRotate(node);
-              }
+            }
+        }
+        return node;
+    }
 
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return c;
     }
 
     public void populate(int[] nums) {
